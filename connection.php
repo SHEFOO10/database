78ff1234007929dd1adf13fa5380b8b418b82490
SHEFOO <sssa2279@gmail.com>
2021-10-30 00:08:29 +0200

    update

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "ec2-3-228-134-188.compute-1.amazonaws.com";
-$username = "zzgfqyxyjejlqa";
-$password = "61439e10bc69cfc075cda07b0eab4e901801e74c93ed9b59f558dc33e092fdd6";
-$data = "dabb86uf0905cq";
-$port = "5432";
+$servername = "nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
+$username = "ppbp5t14e10uui4i";
+$password = "sjbhttch2213ubb4";
+$data = "fgbc02eho5e29ha2";
+$port = "3306";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $data, $port);
 
