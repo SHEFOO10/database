3d369d93812382d43e3b576e773b2ea05a3e2c73
SHEFOO <sssa2279@gmail.com>
2021-10-30 00:29:31 +0200

    My first commit

--- connection.php
+++ connection.php
@@ -3,13 +3,12 @@ $servername = "nnsgluut5mye50or.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	";
 $username = "ppbp5t14e10uui4i";
 $password = "sjbhttch2213ubb4";
 $data = "fgbc02eho5e29ha2";
-$port = "3306";
 // Create connection
-$conn = new mysqli($servername, $username, $password, $data, $port);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failedd: " . $conn->connect_error);  
+  die("Connection failed: " . $conn->connect_error);  
 }
 echo "Connected successfully";
 ?>
