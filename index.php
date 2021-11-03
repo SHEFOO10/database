b647003c29f61e9d9ce76b0850e83ef00a03a523
SHEFOO <sssa2279@gmail.com>
2021-11-03 23:23:30 +0200

    that my portoflio

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <link rel="icon" href="favicon.ico" type="image/icon type">
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
