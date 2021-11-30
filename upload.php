682c35fbfc2771ee5846218c7c1a11571618cb8f
Hamdy Elsayed <sssa2279@gmail.com>
2021-11-30 23:12:30 +0200

    add upload

---
+++ upload.php
@@ -0,0 +1,61 @@
+<?php 
+include 'connection.php';
+$conne = new connection();
+$conne->selectdb();
+#connection string
+$conn = mysqli_connect($conne->localhost,$conne->dbusername,$conne->dbpassword,$conne->dbname);
+ 
+if (isset($_POST["submit"]))
+ {
+     #retrieve file title
+        $title = $_POST["title"];
+     
+    #file name with a random number so that similar dont get replaced
+     $pname = rand(1000,10000)."-".$_FILES["file"]["name"];
+ 
+    #temporary file name to store file
+    $tname = $_FILES["file"]["tmp_name"];
+   
+     #upload directory path
+$uploads_dir = 'images';
+    #TO move the uploaded file to specific location
+    move_uploaded_file($tname, $uploads_dir.'/'.$pname);
+ 
+    #sql query to insert into database
+    $sql = "INSERT into file(url) VALUES('$pname')";
+ 
+    if(mysqli_query($conn,$sql)){
+ 
+    echo "File Sucessfully uploaded";
+    }
+    else{
+        echo "Error";
+    }
+  }  ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>File Upload</title>
+</head>
+<body>
+ 
+<form method="post" enctype="multipart/form-data">
+    <label>Title</label>
+    <input type="text" name="title">
+    <label>File Upload</label>
+    <input type="File" name="file">
+    <input type="submit" name="submit">
+ <?php
+ $sql = "SELECT * FROM file"; 
+ $ree = mysqli_query($conn, $sql);
+ if (mysqli_num_rows($ree) > 0){
+     while($images =mysqli_fetch_assoc($ree)) { ?>
+     <img src="images/<?php echo $images['url'];?>" alt="shefoo">
+ 
+<?php }}?>
+
+</form>
+ 
+</body>
+</html>
+ 
